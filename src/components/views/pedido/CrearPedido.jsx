ac3c88a1870e2976344b3e9dcda99f2bbc4aaa1e
Tomas <tomibudeguer20@gmail.com>
2022-11-09 21:07:50 -0300

    hice el maquetado del formulario para agregar un pedido y editar

--- src/components/views/pedido/CrearPedido.jsx
+++ src/components/views/pedido/CrearPedido.jsx
@@ -1,9 +1,29 @@
+import { Button, Form, Container, Col, Row } from "react-bootstrap";
+
 const CrearPedido = () => {
-    return (
-        <div>
-            form crear pedido
-        </div>
-    );
+  return (
+    <Container className="my-5 mainSection">
+      <h2 className="display-4">Realizar pedido</h2>
+      <hr />
+      <Form>
+        <Form.Group className="mb-3" controlId="formNombreUsuario">
+          <Form.Label>Nombre de usuario</Form.Label>
+          <Form.Control type="text" placeholder="Ej: RollingUser" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formPedido">
+          <Form.Label>Pedido*</Form.Label>
+          <Form.Control type="text" placeholder="Ej: 1 Pizza especial" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formEstado">
+          <Form.Label>Estado</Form.Label>
+          <Form.Control type="text" placeholder="Estado del pedido" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Realizar pedido
+        </Button>
+      </Form>
+    </Container>
+  );
 };
 
 export default CrearPedido;
--- src/components/views/pedido/EditarPedido.jsx
+++ src/components/views/pedido/EditarPedido.jsx
@@ -1,9 +1,34 @@
+import { Button, Form, Container, Col, Row } from "react-bootstrap";
+
 const EditarPedido = () => {
-    return (
-        <div>
-            form editar pedido
-        </div>
-    );
+  return (
+    <Container className="my-5 mainSection">
+      <h2 className="display-4">Editar pedido</h2>
+      <hr />
+      <Form>
+        <Form.Group className="mb-3" controlId="formNombreUsuario">
+          <Form.Label>Nombre de usuario</Form.Label>
+          <Form.Control type="text" placeholder="Ej: RollingUser" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formPedido">
+          <Form.Label>Pedido*</Form.Label>
+          <Form.Control type="text" placeholder="Ej: 1 Pizza especial" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formEstado">
+          <Form.Label>Estado</Form.Label>
+          <Form.Select>
+            <option value="">Seleccione el estado del pedido</option>
+            <option value="Pendiente">Pendiente</option>
+            <option value="En elaboracion">En elaboracion</option>
+            <option value="Listo para retirar">Listo para retirar</option>
+          </Form.Select>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Realizar pedido
+        </Button>
+      </Form>
+    </Container>
+  );
 };
 
 export default EditarPedido;
