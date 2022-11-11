cb88c51ff1b8f5d8d52aceb9f3752c3fe287be52
Tomas <tomibudeguer20@gmail.com>
2022-11-11 10:33:35 -0300

    hice la funcionalidad de la card detalle producto

--- src/components/views/DetalleProducto.jsx
+++ src/components/views/DetalleProducto.jsx
@@ -1,31 +1,61 @@
 import { Card, Badge, Row, Col, Container } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import Swal from "sweetalert2";
+import { obtenerProductoAPI } from "../helpers/queries";
+import { useEffect, useState } from "react";
 
-const DetalleProducto = () => {
-    return (
-        <Container className="my-4 mainSection">
-      <h5 className="text-center display-5 mb-3">Nombre producto</h5>
+const DetalleProducto = ({ producto }) => {
+  const [nombreProducto, setNombreProducto] = useState("");
+  const [precio, setPrecio] = useState("");
+  const [imagen, setImagen] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    obtenerProductoAPI(id).then((respuesta) => {
+      if (respuesta.status === 200) {
+        setNombreProducto(respuesta.dato.nombreProducto);
+        setPrecio(respuesta.dato.precio);
+        setImagen(respuesta.dato.imagen);
+        setCategoria(respuesta.dato.categoria);
+        setDescripcion(respuesta.dato.descripcion);
+      } else {
+        Swal.fire(
+          "Ocurrio un error",
+          "Intente este paso en unos minutos",
+          "error"
+        );
+      }
+    });
+  }, []);
+
+  return (
+    <Container className="my-4 mainSection">
+      <h5 className="text-center display-5 mb-3">{nombreProducto}</h5>
       <Card>
         <Card.Body>
           <Row>
             <Col sm={12} md={4}>
-              <img src='imagen' alt='nombre de la imagen' className="w-100" />
+              <img src={imagen} alt={nombreProducto} className="w-100" />
             </Col>
             <Col sm={12} md={8}>
               <div className="d-flex justify-content-between">
-                <Card.Title className="mt-1 fs-4">Nombre producto</Card.Title>
+                <Card.Title className="mt-1 fs-4">{nombreProducto}</Card.Title>
                 <aside>
-                  <Badge bg="success">Categoria</Badge>
+                  <Badge bg="success">{categoria}</Badge>
                 </aside>
               </div>
               <hr />
-              <Card.Text>Precio:</Card.Text>
-              <Card.Text>Descripcion: </Card.Text>
+              <Card.Text>Precio: ${precio}</Card.Text>
+              <Card.Text>Descripcion: {descripcion}</Card.Text>
             </Col>
           </Row>
         </Card.Body>
       </Card>
     </Container>
-    );
+  );
 };
 
 export default DetalleProducto;
