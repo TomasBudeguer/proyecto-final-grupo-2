04a1c0897614fe131c8ec29a211a6931b1e001bc
Tomas <tomibudeguer20@gmail.com>
2022-11-11 10:55:46 -0300

    hice las queries para los pedidos

--- src/components/helpers/queries.js
+++ src/components/helpers/queries.js
@@ -1,4 +1,7 @@
 const URL = "http://localhost:3004/productos"; //json-server
+const URLpedidos = "http://localhost:3004/pedidos"; //json-server
+
+// PRODUCTOS
 
 export const consultarAPI = async () => {
   try {
@@ -52,6 +55,72 @@ export const obtenerProductoAPI = async (id) => {
 export const editarProductoAPI = async (id, datosActualizados) => {
   try {
     const respuesta = await fetch(URL + "/" + id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(datosActualizados),
+    });
+    return respuesta;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// PEDIDOS
+
+export const consultarPedidosAPI = async () => {
+  try {
+    const respuesta = await fetch(URLpedidos);
+    const listaPedidos = await respuesta.json();
+    return listaPedidos;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const crearPedidoAPI = async (producto) => {
+  try {
+    const respuesta = await fetch(URLpedidos, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(producto),
+    });
+    return respuesta;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const borrarPedidoAPI = async (id) => {
+  try {
+    const respuesta = await fetch(`${URLpedidos}/${id}`, {
+      method: "DELETE",
+    });
+    return respuesta;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const obtenerPedidoAPI = async (id) => {
+  try {
+    const respuesta = await fetch(URLpedidos + "/" + id);
+    const productoBuscado = {
+      dato: await respuesta.json(),
+      status: respuesta.status,
+    };
+    return productoBuscado;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const editarPedidoAPI = async (id, datosActualizados) => {
+  try {
+    const respuesta = await fetch(URLpedidos + "/" + id, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
