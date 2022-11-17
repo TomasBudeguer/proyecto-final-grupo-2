4afd16396fb745917cdb146dff680eca3d403d0d
JLGM <juss.jlgm@gmail.com>
2022-11-17 19:02:41 -0300

    se agrego la logica de ruta protegida para usuario

--- src/components/routes/RutasProtegidas.jsx
+++ src/components/routes/RutasProtegidas.jsx
@@ -1,14 +1,16 @@
 import { Navigate } from "react-router-dom";
+import { userAdmin } from "../helpers/queriesUsuarios";
 
 
 
 const RutasProtegidas = ({children}) => {
     const token = JSON.parse(localStorage.getItem('tokenUsuario')) || null
+    console.log(token)
     //esta vacio el token?
-    if(!token){
+    if(!token || token.email != userAdmin.email){
         // si esta vacio
         return <Navigate to={'/login'}></Navigate>
-return 
+
     }else{
         //si estamos logueados correctamente
         return children;
