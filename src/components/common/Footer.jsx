cdaa3484acf53628d5c873e240aed044f183bf02
Nicolas Gomez Marigliano <rodnicgm@gmail.com>
2022-11-21 15:49:18 -0300

    agregado de nuevos estilos

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,6 +1,17 @@
 const Footer = () => {
   return (
     <footer className="p-4 bg-light">
+      <section className="d-flex">
+        <div>
+        
+        </div>
+        <div>
+          
+        </div>
+        <div>
+          
+        </div>
+      </section>
       <p className="text-center"> &copy; Todos los derechos reservados</p>
     </footer>
   );
--- src/components/views/AboutUs.jsx
+++ src/components/views/AboutUs.jsx
@@ -4,15 +4,22 @@ import "../aboutUs.css";
 const AboutUs = () => {
   return (
     <div className="backgroundGral mainSection">
-      <Container className="d-flex mediumDevice">
-        <img
-          src="https://i.ibb.co/Tg1d724/primos.jpg"
-          class="imagenPrimos mediumDevice mx-2"
-        ></img>
-        <section classname="mediumDevice">
-          <p class="lh-lg textoCentrado ">
-            Proyecto final Curso de FullStack, dictado por RollingCode.
-            <p class="lh-lg textoJustificado">
+      <Container>
+        <Row>
+          <Col sm={12} md={6} lg={4}>
+            <img
+              src="https://i.ibb.co/Tg1d724/primos.jpg"
+              className="w-100 imagenPrimos my-4"
+              alt="fotoprimos"
+            ></img>
+          </Col>
+
+          <Col sm={12} md={6} lg={8}>
+            <div>
+            <p className="lh-lg textoCentrado my-4">
+              Proyecto final Curso de FullStack, dictado por RollingCode.
+            </p>
+            <p className="lh-lg textoJustificado">
               Bienvenidos a nuestro sitio, somos el grupo 2. Previo a contarles
               acerca de nosotros, queremos destacar que a lo largo de la
               creacion este proyecto pudimos ir concatenando los distintos
@@ -20,29 +27,32 @@ const AboutUs = () => {
               intencion de generar agrado a la vista, invitando al usuario en
               permanecer en el sitio, utilizando colores recomendados a tal fin.
             </p>
-          </p>
-          <p class="lh-lg textoJustificado">
-            
-            The KITCHEN es el restaurant que siempre soñaste, con nuestras
-            modalidades de Delivery, Take Away! y nuestras sucursales a lo largo
-            de la provincia, te invitan a redescubrir la gastronomia de la mano de los
-            mejores Cheff e ingredientes del mundo.
-          </p>
-          <p class="lh-lg textoJustificado">
-            Contamos con 5 sucursales a lo largo de la provincia para tu
-            comodidad:
-          </p>
-          <ul class="lh-lg">
-            <li>Av. Mate de Luna 1890</li>
-            <li>Av. Siempre Viva 742</li>
-            <li>General Paz 576</li>
-            <li>Calle Oeste, Tafi Viejo</li>
-            <li>Av. Peron 3200</li>
-          </ul>
-          <p class="lh-lg textoJustificado">
-             The KITCHEN surgio con la iniciativa de 03 socios, Tomas Budeguer, Jose Luis Gomez Marigliano y Nicolas Gomez Marigliano (de derecha a izquierda), quienes tomaron como iniciativa, ofrecer la mejor calidad en conmida, en el menor tiempo posible.
+            <p className="lh-lg textoJustificado">
+              The KITCHEN es el restaurant que siempre soñaste, con nuestras
+              modalidades de Delivery, Take Away! y nuestras sucursales a lo
+              largo de la provincia, te invitan a redescubrir la gastronomia de
+              la mano de los mejores Cheff e ingredientes del mundo.
             </p>
-        </section>
+            <p className="lh-lg textoJustificado">
+              Contamos con 5 sucursales a lo largo de la provincia para tu
+              comodidad:
+            </p>
+            <ul className="lh-lg">
+              <li>Av. Mate de Luna 1890</li>
+              <li>Av. Siempre Viva 742</li>
+              <li>General Paz 576</li>
+              <li>Calle Oeste, Tafi Viejo</li>
+              <li>Av. Peron 3200</li>
+            </ul>
+            <p className="lh-lg textoJustificado">
+              The KITCHEN surgio con la iniciativa de 03 socios, Tomas Budeguer,
+              Jose Luis Gomez Marigliano y Nicolas Gomez Marigliano (de derecha
+              a izquierda), quienes tomaron como iniciativa ofrecer la mejor
+              calidad en comida, al mejor precio y en el menor tiempo posible.
+            </p>
+            </div>
+          </Col>
+        </Row>
       </Container>
     </div>
   );
